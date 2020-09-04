ef26f62a0c63bb34829036476f007fb76edb7a24
maryadipoipo187@gmail.com <maryadipoipo187@gmail.com>
2020-09-04 19:21:05 +0700

    Create new model and controller for profile

--- database/migrations/2020_09_04_114510_add_new_column_to_users.php
+++ database/migrations/2020_09_04_114510_add_new_column_to_users.php
@@ -14,6 +14,7 @@ class AddNewColumnToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes();
             $table->string('username');
             $table->json('skill_ids');
             $table->integer('profile_id')->unsigned();
